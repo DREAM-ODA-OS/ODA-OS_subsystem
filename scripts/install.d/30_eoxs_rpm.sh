53217f0e8fabc5c94334e4a475bb185622f6023d
Martin Paces <martin.paces@eox.at>
2014-02-16 17:27:35 +0100

    EOxServer - manual installation

--- scripts/install.d/30_eoxs_rpm.sh
+++ scripts/install.d/30_eoxs_rpm.sh
@@ -6,8 +6,13 @@
 
 . `dirname $0`/../lib_logging.sh  
 
-info "Installing EOxServer ... "
+#info "Installing EOxServer ... "
+info "Installing EOxServer Dependencies ... "
+# NOTE: as long as there is on 0.4 RPM on-line we go for 
+#       manual EOxServer install. We need the dependencies 
+#       though 
 
 #======================================================================
 
-yum --assumeyes install EOxServer proj-epsg
+#yum --assumeyes install EOxServer proj-epsg
+yum --assumeyes install proj-epsg fcgi gd libXpm libxml2-python mapserver mapserver-python
