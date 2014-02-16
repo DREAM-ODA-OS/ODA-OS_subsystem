b07d84ea8e1caa2a35d74d68c43777723260dd7e
Martin Paces <martin.paces@eox.at>
2014-02-16 20:16:22 +0100

    Itegrating the Ingestion Engine as a system service.

--- scripts/install.d/45_ie_config.sh
+++ scripts/install.d/45_ie_config.sh
@@ -47,8 +47,6 @@ MNGCMD="${INSTROOT}/manage.py"
 #-------------------------------------------------------------------------------
 # configuration 
 
-set -x 
-
 # ingestion_config.json 
 sudo -u "$ODAOSUSER" ex "$INGESTION_CONFIG" <<END
 1,\$s:\("DownloadManagerDir"[	 ]*\:[	 ]*"\).*\("[	 ]*,\):\1$ODAOS_DM_HOME\2:
@@ -72,3 +70,222 @@ sudo -u "$ODAOSUSER" python "$MNGCMD" collectstatic --noinput
 
 # setup new database 
 sudo -u "$ODAOSUSER" python "$MNGCMD" syncdb --noinput 
+
+#======================================================================
+# wrapper script
+
+#IE_CONSOLE_LOG="/dev/null"
+IE_CONSOLE_LOG="$ODAOSLOGDIR/ingeng_console.log"
+IE_DAEMON="${INSTROOT}/start-ie-as-daemon.sh"
+
+info "Cretating the Ingestion Engine's daemon start-up script: $IE_DAEMON"
+
+# make the necessary changes
+cat >"$IE_DAEMON" <<END
+#!/bin/sh
+#
+# Ingestion Engine start-up script 
+#
+IE_HOME="\$( dirname \$0 )"
+IE_HOME="\$( cd \$IE_HOME ; pwd ; )"
+IE_ADDR=\${IE_ADDR:-0.0.0.0}
+IE_PORT=\${IE_PORT:-8000}
+IE_OPT="--nothreading --noreload \\"\$IE_ADDR:\$IE_PORT\\""
+
+if [ -z "\$IE_USER" ]
+then 
+    EXEC="/bin/sh"
+else 
+    EXEC="runuser "\$IE_USER" -s /bin/sh"
+fi
+
+PID=\$( \$EXEC -c "ulimit -S -c 0 ; cd \\"\$IE_HOME\\" ; /usr/bin/python $MNGCMD runserver \$IE_OPT >'$IE_CONSOLE_LOG' 2>&1 & echo \\\$!" )
+
+# check whether the daemon is still alive 
+sleep 2 
+[ -z "\$PID" ] && exit 1 
+ps p "\$PID" >/dev/null 2>&1 || exit 1 
+
+# IE_PIDFILE optionally contains file-name where the daemons PID shall be written.
+if [ -n "\$IE_PIDFILE" ]
+then
+    echo "\$PID" > "\$IE_PIDFILE"
+else
+    echo "Daemon started. PID=\$PID"
+fi
+END
+
+chown "$ODAOSUSER:$ODAOSGROUP" "$IE_DAEMON"
+chmod 0755 "$IE_DAEMON" 
+
+#======================================================================
+# init script  
+
+IE_INIT="/etc/init.d/ingeng"
+IE_PIDFILE="/var/run/ingend.pid"
+IE_LOCK="/var/lock/subsys/ingeng"
+
+info "Cretating the Ingestion Engine's init script: $IE_INIT"
+
+cat >"$IE_INIT" <<END 
+#!/bin/sh 
+# 
+# ingeng        Ingestion Engine 
+#
+# chkconfig: - 85 15
+# description: starts, stops, and restarts the donwload manager 
+#
+# NOTE: This init script is created by the DREAM ODA-OS installation script.
+# NOTE: The service must be explicitely enabled by 'chkconfig ingeng on' command.
+#
+# pidfile: $IE_PIDFILE
+# lockfile: $IE_LOCK
+#
+### BEGIN INIT INFO
+# Provides: ngeo-dm 
+# Required-Start: \$local_fs \$remote_fs \$network \$named
+# Required-Stop: \$local_fs \$remote_fs \$network
+# Default-Stop: 0 1 2 3 4 5 6
+# Default-Start: 
+# Short-Description: Starts, stops, and restarts the ngEO donwload manager 
+# Description: Starts, stops, and restarts the ngEO donwload manager
+### END INIT INFO
+
+# Source function library.
+. /etc/rc.d/init.d/functions
+
+prog="ingeng"
+user="$ODAOSUSER"
+ingengd="$IE_DAEMON"
+pidfile="$IE_PIDFILE"
+lockfile="$IE_LOCK"
+timeout="5"
+
+
+# create PID-file directory if it does not exists 
+[ -d "\$(dirname \${pidfile})" ] || mkdir -p "\$(dirname \${pidfile})"
+
+#---------
+ie_status()
+{ 
+    if [ -f "\${pidfile}" ]
+    then 
+
+        pid="\$( cat "\${pidfile}" )"
+
+        if ps p "\$pid" >/dev/null 
+        then 
+            echo $"\${prog} (pid \$pid) is running ..."
+            return 0 
+        else 
+            echo $"\${prog} is dead but pid file exists"
+            return 1
+        fi 
+
+    elif [ -f "\${lockfile}" ]
+    then 
+
+        echo $"\${prog} is dead but subsys locked" 
+        return 2 
+
+    else 
+
+        echo $"\${prog} is stopped"
+        return 3 
+
+    fi 
+} 
+
+#---------
+ie_start() 
+{ 
+    # parameters of the daemon's statup script
+    export IE_PIDFILE="\$pidfile"
+    export IE_USER="\$user"
+
+    # check status and write message if something's wrong 
+    MSG=\$( ie_status )
+    case "\$?" in 
+        0 | 1 | 2 ) echo $"WARNING: \$MSG" ;; 
+    esac 
+
+    echo -n \$"Starting \$prog: "
+
+    # start the daemon 
+    daemon --pidfile=\${pidfile} \${ingengd}
+    RETVAL=\$?
+
+    echo
+    [ \$RETVAL = 0 ] && touch \${lockfile}
+    return \$RETVAL
+} 
+
+#---------
+ie_stop() 
+{ 
+    # check status and write message if something's wrong 
+    MSG=\$( ie_status )
+    case "\$?" in 
+        1 | 2 | 3 ) echo $"WARNING: \$MSG" ;; 
+    esac 
+
+    echo -n \$"Stopping \$prog: "
+    killproc -p \${pidfile} -d \${timeout} \${ingengd}
+    RETVAL=\$?
+    echo 
+    [ \$RETVAL = 0 ] && rm -f \${lockfile} \${pidfile}
+} 
+
+#---------
+ie_reset() 
+{
+    # check status and write message if something's wrong 
+    MSG=\$( ie_status )
+    case "\$?" in 
+        0 | 1 | 2 )
+            echo $"WARNING: \$MSG" 
+            echo $"ERROR: Stop \$prog properly before the DB reset!"
+            return 3 
+            ;; 
+    esac 
+    echo -n \$"Resetting \$prog: "
+}
+
+#---------
+case "\$1" in 
+
+    start) ie_start ;; 
+
+    stop) ie_stop ;; 
+
+    restart) ie_stop ; ie_start ;; 
+
+    status)
+        ie_status 
+        RETVAL=\$?
+        ;; 
+
+    reset) 
+        ie_reset
+        RETVAL=\$?
+        ;; 
+    *) 
+        echo \$"Usage: \$prog {start|stop|restart|reset|status}"
+        RETVAL=2
+        ;;
+esac
+
+exit \$RETVAL
+END
+
+chmod 0755 "$IE_INIT" 
+
+#add service for managemnt by chkconfig
+chkconfig --add ingeng
+
+#======================================================================
+# make the donwload manager enabled permanently and start the service 
+
+info "Enabling the download manager's service ..."
+chkconfig ingeng on 
+service ingeng restart
