d7caa9d2b4f135af90d28b71a2d57bffceda5f18
martin.paces <martin.paces@eox.at>
2014-05-22 16:47:12 +0200

    Fixing the DQ installation and configuration.

--- scripts/install.d/35_data_quality_install.sh
+++ scripts/install.d/35_data_quality_install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #
-# install Data Quality subsystem 
+# install Data Quality subsystem
 #
 #======================================================================
 
-. `dirname $0`/../lib_logging.sh  
+. `dirname $0`/../lib_logging.sh
 
 info "Installing Data Quality subsytem ... "
 
@@ -18,85 +18,96 @@ DQ_TMPDIR='/tmp/data-quality'
 [ -z "$ODAOSGROUP" ] && error "Missing the required ODAOSGROUP variable!"
 [ -z "$ODAOSHOSTNAME" ] && error "Missing the required ODAOSHOSTNAME variable!"
 
-if [ -d "$ODAOS_DQ_HOME" ] 
-then 
+if [ -d "$ODAOS_DQ_HOME" ]
+then
     error "Data Quality subsytem seems to be already installed in: $ODAOS_DQ_HOME"
     error "Data Quality subsytem installation is terminated."
-    exit 0 
+    exit 0
 fi
 
 #======================================================================
-# setup automatic cleanup 
+# setup automatic cleanup
 
 REMOVE_NETRC_BACKUP=FALSE
 RESTORE_NETRC_BACKUP=FALSE
 
-on_exit() 
-{ 
-    [ "$REMOVE_NETRC_BACKUP" == TRUE ] && rm -fv "$HOME/.netrc" 
-    [ "$RESTORE_NETRC_BACKUP" == TRUE ] && mv -fv "$HOME/.netrc.bak" "$HOME/.netrc" 
+on_exit()
+{
+    [ "$REMOVE_NETRC_BACKUP" == TRUE ] && rm -fv "$HOME/.netrc"
+    [ "$RESTORE_NETRC_BACKUP" == TRUE ] && mv -fv "$HOME/.netrc.bak" "$HOME/.netrc"
 
     [ -d "$DQ_TMPDIR" ] && rm -fR "$DQ_TMPDIR"
-} 
+}
 
-trap on_exit EXIT 
+trap on_exit EXIT
 
 #======================================================================
 # locate a download manager tarball in the DM directory
 
-# fixed source URL 
-DQ_URL='ftp://dream-reader@ftp.spacebel.be/Software deliveries/Task13-ASV/QSS/InitialVersion_7May14_SS_FAT Delivery/DREAM-QSS-V1.0-7may14.tgz'
-DQ_TARBALL="$CONTRIB/`basename "$DQ_URL"`"
+# fixed source URL
+#DQ_URL='ftp://dream-reader@ftp.spacebel.be/Software deliveries/Task13-ASV/QSS/InitialVersion_7May14_SS_FAT Delivery/DREAM-QSS-V1.0-7may14.tgz'
+#DQ_URL='ftp://dream-reader@ftp.spacebel.be/Inbox/ASV%20GEO/QSS/0.2/qss-0.2.tgz'
+DQ_URL='ftp://dream-reader@ftp.spacebel.be/Inbox/ASV%20GEO/QSS/0.2/qss.tgz'
+#DQ_TARBALL="$CONTRIB/`basename "$DQ_URL"`"
+DQ_TARBALL="$CONTRIB/qss-0.2.1.tgz"
 
-if [ -f "$DQ_TARBALL" ] 
-then 
-    # found - using local copy  
+if [ -f "$DQ_TARBALL" ]
+then
+    # found - using local copy
     info "Using the existing local copy of the Data Quality installation package."
 
-else 
-    # not found - donwload the package 
+else
+    # not found - download the package
 
-    # try to use temporarily .netrc in contrib directory 
+    # try to use temporarily .netrc in contrib directory
     if [ -f "$CONTRIB/.netrc" ]
-    then 
-        # backup existing .netrc file 
+    then
+        # backup existing .netrc file
 
         if [ -f "$HOME/.netrc" ]
-        then 
+        then
             mv -fv "$HOME/.netrc" "$HOME/.netrc.bak"
             RESTORE_NETRC_BACKUP=TRUE
-        fi 
+        fi
 
         cp "$CONTRIB/.netrc" "$HOME/.netrc"
         chmod 0600 "$HOME/.netrc"
         REMOVE_NETRC_BACKUP=TRUE
-    fi 
+    fi
 
     info "Donwloading the Data Quality installation package ... "
     info "$DQ_URL -> $DQ_TARBALL"
     curl -n -s -S "$DQ_URL" -o "$DQ_TARBALL"
     info "Download completed."
 
-fi 
+fi
 
 info "$DQ_TARBALL"
 
 #======================================================================
-# unpack the download manager 
+# unpack the download manager
 
-# clean-up any existing stuff  
+# clean-up any existing stuff
 [ -d "$ODAOS_DQ_HOME" ] && rm -fR "$ODAOS_DQ_HOME"
 [ -d "$DQ_TMPDIR" ] && rm -fR "$DQ_TMPDIR"
 
 mkdir -p "$DQ_TMPDIR"
 tar -xzf "$DQ_TARBALL" --directory="$DQ_TMPDIR"
 
-[ -d "$DQ_TMPDIR/dream" ] || error "Cannot find the '$DQ_TMPDIR/dream' directory!"
 
-# move to the destination 
-mv -f "$DQ_TMPDIR/dream" "$ODAOS_DQ_HOME"
+[ 1 -eq `find "$DQ_TMPDIR" -mindepth 1 -maxdepth 1 -type d | wc -l` ] || {
+    ls -la "$DQ_TMPDIR"
+    error "Cannot locate a suitable installation directory!"
+}
 
-# fix permisions 
+DQ_TMP_SRC="`find "$DQ_TMPDIR" -mindepth 1 -maxdepth 1 -type d`"
+
+[ -d "$DQ_TMP_SRC" ] || error "Cannot find the '$DQ_TMP_SRC' directory!"
+
+# move to the destination
+mv -f "$DQ_TMP_SRC" "$ODAOS_DQ_HOME"
+
+# fix permisions
 chown -R "$ODAOSUSER:$ODAOSGROUP" "$ODAOS_DQ_HOME"
 
 info "Data Quality subsytem installed to: $ODAOS_DQ_HOME"
--- scripts/install.d/60_data_quality.sh
+++ scripts/install.d/60_data_quality.sh
@@ -1,50 +1,90 @@
 #!/bin/sh
 #
-# configure Data Quality subsystem 
+# configure Data Quality subsystem
 #
 #======================================================================
 
-. `dirname $0`/../lib_logging.sh  
+. `dirname $0`/../lib_logging.sh
 
 info "Configuring Data Quality subsytem ... "
 
 #======================================================================
 
+[ -z "$ODAOS_DQ_HOME" ] && error "Missing the required ODAOS_DQ_HOME variable!"
+[ -z "$ODAOSHOSTNAME" ] && error "Missing the required ODAOSHOSTNAME variable!"
+[ -z "$ODAOSLOGDIR" ] && error "Missing the required ODAOSLOGDIR variable!"
+[ -z "$ODAOSUSER" ] && error "Missing the required ODAOSUSER variable!"
+[ -z "$ODAOSGROUP" ] && error "Missing the required ODAOSGROUP variable!"
+[ -z "$ODAOSDATADIR" ] && error "Missing the required ODAOSDATADIR variable!"
+
+DQ_USER="$ODAOSUSER"
+DQ_GROUP="$ODAOSGROUP"
+DQ_LOG_DIR="$ODAOSLOGDIR/data-quality"
+DQ_DATA_DIR="$ODAOSDATADIR/data-quality"
 DQ_WPS_CONTEXT="ool-wps-server"
 DQ_SERVICE_HOST="services.spotimage.fr"
 DQ_SERVICE_PORT=8443
 DQ_PROXY_HOST="127.0.0.1"
 DQ_PROXY_PORT=8080
 ODAOS_PORT=80
-DQ_SERVICE="tomcat7-dq"
+DQ_SERVICE="tomcat-dq"
 
-[ -z "$ODAOS_DQ_HOME" ] && error "Missing the required ODAOS_DQ_HOME variable!"
-[ -z "$ODAOSHOSTNAME" ] && error "Missing the required ODAOSHOSTNAME variable!"
-
-if [ ! -d "$ODAOS_DQ_HOME" ] 
-then 
+if [ ! -d "$ODAOS_DQ_HOME" ]
+then
     error "Data Quality subsytem does not seem to be installed in: $ODAOS_DQ_HOME"
     error "Data Quality subsytem configuration is terminated."
-    exit 0 
+    exit 0
 fi
+
+#======================================================================
+
+info "Preparing the log directory: $DQ_LOG_DIR"
+
+[ -d "$DQ_LOG_DIR" -o -f "$DQ_LOG_DIR" ] && rm -vfR "$DQ_LOG_DIR"
+
+mkdir -vp "$DQ_LOG_DIR"
+chown -v "$DQ_USER:$DQ_GROUP" "$DQ_LOG_DIR"
+chmod -v 0755 "$DQ_LOG_DIR"
+
+info "Preparing the data directory: $DQ_DATA_DIR"
+
+[ -d "$DQ_DATA_DIR" -o -f "$DQ_DATA_DIR" ] && rm -vfR "$DQ_DATA_DIR"
+
+mkdir -vp "$DQ_DATA_DIR"
+chown -v "$DQ_USER:$DQ_GROUP" "$DQ_DATA_DIR"
+chmod -v 0755 "$DQ_DATA_DIR"
 
 #======================================================================
 
 info "Data Quality subsytem configuration ..."
 
-[ -f "$ODAOS_DQ_HOME/install.sh" ] || error "Cannot find the '$ODAOS_DQ_HOME/install.sh' installer!"
+DQ_INSTALLER="$ODAOS_DQ_HOME/q2/install.sh"
 
+[ -f "$DQ_INSTALLER" ] || error "Cannot find the '$DQ_INSTALLER' installer!"
 
-sudo -u "$ODAOSUSER" sh "$ODAOS_DQ_HOME/install.sh" "$DQ_PROXY_HOST" "$DQ_PROXY_PORT" \
-        "$ODAOSHOSTNAME" "$ODAOS_PORT" "$DQ_SERVICE_HOST" "$DQ_SERVICE_PORT" "$DQ_WPS_CONTEXT"
+# NOTE: The install script requires the working directory
+#       to be the same as the location of the script.
+(
+    cd "`dirname "$DQ_INSTALLER" `"
+    sudo -u "$DQ_USER" sh -x "./`basename "$DQ_INSTALLER" `" \
+            -t "$DQ_PROXY_HOST" -u "$DQ_PROXY_PORT" \
+            -j "http" -k "$ODAOSHOSTNAME" -l "$ODAOS_PORT" \
+            -p "$DQ_SERVICE_HOST" -q "$DQ_SERVICE_PORT" -r "$DQ_WPS_CONTEXT" \
+            -a "$DQ_LOG_DIR" -b "$DQ_DATA_DIR"
+)
 
 #======================================================================
-# start the service 
+# start the service
 
 info "Data Quality subsytem service initialization ..."
+DQ_STARTUP_SRC="$ODAOS_DQ_HOME/q2/$DQ_SERVICE"
+sudo -u "$DQ_USER" ex -V "$DQ_STARTUP_SRC" <<END
+1,\$s/^\(CATALINA_USER=\).*$/\1"$DQ_USER"/
+wq
+END
 
-cp "$ODAOS_DQ_HOME/$DQ_SERVICE" "/etc/init.d/$DQ_SERVICE"
+cp -fv "$DQ_STARTUP_SRC" "/etc/init.d"
 
-chkconfig "$DQ_SERVICE" on 
+chkconfig "$DQ_SERVICE" on
 service "$DQ_SERVICE" restart
 
