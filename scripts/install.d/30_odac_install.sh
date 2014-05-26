d0c79a95298ae89832223c675661fa16cbf8585b
martin.paces <martin.paces@eox.at>
2014-05-26 14:44:11 +0200

    ODAClient upgrade

--- scripts/install.d/30_odac_install.sh
+++ scripts/install.d/30_odac_install.sh
@@ -76,7 +76,7 @@ then
     #URL="`get_release_url`"
 
     #fixed version download
-    URL="https://github.com/DREAM-ODA-OS/ODAClient/releases/download/0.4.1/ODAClient-0.4.1.tgz"
+    URL="https://github.com/DREAM-ODA-OS/ODAClient/releases/download/0.4.2/ODAClient-0.4.2.tgz"
 
     info "Downloading from: $URL"
 
