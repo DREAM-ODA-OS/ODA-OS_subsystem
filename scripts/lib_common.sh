57221f2fa9bb114655ce1586b0c8c9ced156f8c4
martin.paces <martin.paces@eox.at>
2014-06-20 16:20:35 +0200

    Version update.

--- scripts/lib_common.sh
+++ scripts/lib_common.sh
@@ -28,7 +28,7 @@
 #-------------------------------------------------------------------------------
 
 # version 
-export ODAOSVERSION=0.2.1
+export ODAOSVERSION=0.2.4
 
 # public hostname (or IP number) under which the ODA-OS shall be accessable 
 # NOTE: Critical parameter! Be sure you set to proper value.
