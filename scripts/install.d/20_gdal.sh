b0263a4881cc5f8bd5b492d7793d967e9e640372
Martin Paces <martin.paces@eox.at>
2014-08-05 23:21:22 +0200

    Fixing GDAL installation script.

--- scripts/install.d/20_gdal.sh
+++ scripts/install.d/20_gdal.sh
@@ -9,25 +9,26 @@
 info "Installing GDAL library ... "
 
 #======================================================================
-## do not use the testing repository
-#ex /etc/yum.repos.d/eox-testing.repo <<END
-#/\[eox-testing\]
-#/^[ 	]*exclude[ 	]*=.*\$/
-#s/^\([ 	]*exclude[ 	]*=.*\)\$/\1 gdal-eox*/
-#/\[eox-testing-source\]
-#/^[ 	]*exclude[ 	]*=.*\$/
-#s/^\([ 	]*exclude[ 	]*=.*\)\$/\1 gdal-eox*/
-#/\[eox-testing-noarch\]
-#/^[ 	]*exclude[ 	]*=.*\$/
-#s/^\([ 	]*exclude[ 	]*=.*\)\$/\1 gdal-eox*/
-#wq
 #END
+# use the testing repository
+{ ex /etc/yum.repos.d/eox-testing.repo || true ; } <<END
+1,\$s/ gdal-eox\*//
+/\[eox-testing\]
+/^[ 	]*includepkgs[ 	]*=.*\$/
+s/^\([ 	]*includepkgs[ 	]*=.*\)\$/\1 gdal-eox*/
+/\[eox-testing-source\]
+/^[ 	]*includepkgs[ 	]*=.*\$/
+s/^\([ 	]*includepkgs[ 	]*=.*\)\$/\1 gdal-eox*/
+/\[eox-testing-noarch\]
+/^[ 	]*includepkgs[ 	]*=.*\$/
+s/^\([ 	]*includepkgs[ 	]*=.*\)\$/\1 gdal-eox*/
+wq
+END
 
 # reset yum cache
-#yum clean all
+yum clean all
 
-yum --assumeyes install gdal-eox gdal-eox-python \
-    gdal-eox-driver-dimap gdal-eox-driver-envisat \
-    gdal-eox-driver-netcdf gdal-eox-driver-openjpeg2 \
-    proj-epsg
-
+rpm -ev --nodeps `rpm -qa | grep '^gdal'` || true
+yum --assumeyes install gdal-eox gdal-eox-python proj-epsg
+yum --assumeyes install gdal-eox-driver-dimap gdal-eox-driver-envisat \
+    gdal-eox-driver-netcdf gdal-eox-driver-openjpeg2
