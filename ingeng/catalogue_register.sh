2d47cce58c99e06896a4a6f860dde42901c167b4
Martin Paces <martin.paces@eox.at>
2014-09-18 14:14:32 +0200

    Catalogue registration bugfix.
    Assuring readability of the input metadata by creating
    a temporary file.

--- ingeng/catalogue_register.sh
+++ ingeng/catalogue_register.sh
@@ -35,7 +35,13 @@ DIR="`dirname "$MANIFEST"`"
 DIR="`_expand "$DIR"`"
 
 META="`cat "$MANIFEST" | sed -ne 's/^METADATA_EOP20="\(.*\)"/\1/p' | _pipe_expand "$DIR"`"
-QUOTED="`python -c "from urllib import quote_plus; print quote_plus('$META'),"`"
+
+#make sure the file is readable by the tomcat
+_tmp="`mktemp`.xml"
+trap "_remove '$_tmp'" EXIT
+cat "$META" > "$_tmp"
+
+QUOTED="`python -c "from urllib import quote_plus; print quote_plus('$_tmp'),"`"
 
 URL="http://127.0.0.1/excat2/csw?request=Harvest&service=CSW&version=2.0.2&namespace=xmlns(csw=http://www.opengis.net/cat/csw)&source=$QUOTED&resourceFormat=application/xml&resourceType=http://www.opengis.net/eop/2.0"
 
