1e3ea571b128c370aaeb1e1973a88f78771b860a
Martin Paces <martin.paces@eox.at>
2014-10-06 19:31:27 +0200

    Updating catalogue registration scritps.
    - Repeated registration updates the existing metadata records (remove+new-insert).
    - Catalogue response interpretation and non-zero return code for failures.
    - Manifest passed view std.in is captured to a temporary file.
    - Fixed temporary file naming and removal.

--- ingeng/catalogue_deregister.sh
+++ ingeng/catalogue_deregister.sh
@@ -2,33 +2,52 @@
 #
 #  Deregister product's metadata form a catalogue.
 #
-# USAGE: 
+# USAGE:
 #  catalogue_deregister.sh <identifier> [TBC]
 #
-# DESCRIPTION: 
+# DESCRIPTION:
 #  Deregister product metadata form a catalogue. The product
 #  is specified by the provided identifier.
 #
-# NOTE: 
+# NOTE:
 #  This script is not invoked directly by the Ingestion Engine.
 #
 
 . "`dirname $0`/lib_common.sh"
 
+_rsp="`mktemp --suffix=.catreg`"
+trap "_remove '$_rsp'" EXIT
+
 info "Catalogue metadata deregistration started ..."
 
-ID=$1
-[ -z "$ID" ] && { error "Missing the required product identifier!" ; exit 1 ; }
+IDENTIFIER=$1
+[ -z "$IDENTIFIER" ] && { error "Missing the required product identifier!" ; exit 1 ; }
 
-info "    IDENTIFIER: '$ID'"
+info "    IDENTIFIER: '$IDENTIFIER'"
 
-QUOTED="`python -c "from urllib import quote_plus; print quote_plus('$ID'),"`"
+QUOTED="`python -c "from urllib import quote_plus; print quote_plus('$IDENTIFIER'),"`"
 
 URL="http://127.0.0.1/excat2/csw?request=Transaction&service=CSW&version=2.0.2&namespace=xmlns(csw=http://www.opengis.net/cat/csw/2.0.2)&transactiontype=delete&constraint=apeop:identifier=%27$QUOTED%27&constraintLanguage=CQL_TEXT&constraint_language_version1.1.0&typeName=eop:EarthObservation"
 
 info "Deregistering product ..."
 info "$URL"
-curl -s -S $URL 2>&1 | while read L
-do
-    info "$L"
-done
+curl -s -S $URL 2>&1 | tee "$_rsp" | info_pipe
+_root="`xml_extract.py "$_rsp" './name()'`"
+
+if [ '{http://www.opengis.net/cat/csw/2.0.2}TransactionResponse' == "$_root" ]
+then
+    _nprod="`xml_extract.py "$_rsp" '//{http://www.opengis.net/cat/csw/2.0.2}totalDeleted/text()'`"
+    if [ "$_nprod" -eq "1" ]
+    then
+        info "Product metadata record removed successfully from the catalogue."
+        exit 0
+    elif [ "$_nprod" -eq "0" ]
+    then
+        warn "No metadata record removed!"
+        warn "There is no metadata record of product $IDENTIFIER registered to the catalogue!"
+        exit 0
+    fi
+fi
+
+error "Failed to deregister the catalogue metadata record of product $IDENTIFIER!"
+exit 1
--- ingeng/catalogue_register.sh
+++ ingeng/catalogue_register.sh
@@ -26,8 +26,19 @@
 . "`dirname $0`/lib_common.sh"
 
 info "Catalogue metadata registration ..."
- 
-MANIFEST=$1
+
+_manifest="`mktemp --suffix=.manifest`"
+_rsp="`mktemp --suffix=.catreg`"
+trap "_remove '$_tmp' '$_rsp' '$_manifest'" EXIT
+
+if [ "$1" == '-' ]
+then
+    cat "$1" > "$_manifest"
+    MANIFEST="$_manifest"
+else
+    MANIFEST="$1"
+fi
+cat $MANIFEST
 [ -z "$MANIFEST" ] && { error "Missing the required manifest file!" ; exit 1 ; }
 get_field() { sed -ne "s/^$1=\"\(.*\)\"/\1/p" "$MANIFEST" ;}
 get_path() { get_field "$1" | _pipe_expand "$DIR" ;}
@@ -39,19 +50,39 @@ DIR="`_expand "$DIR"`"
 META="`get_path METADATA_EOP20`"
 IDENTIFIER="`get_field IDENTIFIER`"
 [ -n "$IDENTIFIER" ] || IDENTIFIER="`basename "$META".xml`"
+_tmp="/tmp/$IDENTIFIER.xml"
 
 #make sure the file is readable by the tomcat
-_tmp="/tmp/$IDENTIFIER.xml"
-trap "_remove '$_tmp'" EXIT
 cat "$META" > "$_tmp"
 
 QUOTED="`python -c "from urllib import quote_plus; print quote_plus('$_tmp'),"`"
 
 URL="http://127.0.0.1/excat2/csw?request=Harvest&service=CSW&version=2.0.2&namespace=xmlns(csw=http://www.opengis.net/cat/csw)&source=$QUOTED&resourceFormat=application/xml&resourceType=http://www.opengis.net/eop/2.0"
 
-info "Registering product ..."
+info "Registering product to the catalogue ..."
 info "$URL"
-curl -s -S $URL 2>&1 | while read L 
-do
-    info "$L"
-done
+curl -s -S $URL 2>&1 | tee "$_rsp" | info_pipe
+_root="`xml_extract.py "$_rsp" './name()'`"
+# check the response type
+if [ '{http://www.opengis.net/ows/2.0}ExceptionReport' == "$_root" ]
+then
+    info "Removing the existing metadata record ..."
+    "`dirname $0`/catalogue_deregister.sh" "$IDENTIFIER" || exit 1
+    info "Second attempt ..."
+    info "$URL"
+    curl -s -S $URL 2>&1 | tee "$_rsp" | info_pipe
+    _root="`xml_extract.py "$_rsp" './name()'`"
+fi
+
+if [ '{http://www.opengis.net/cat/csw/2.0.2}HarvestResponse' == "$_root" ]
+then
+    _nprod="`xml_extract.py "$_rsp" '//{http://www.opengis.net/cat/csw/2.0.2}totalInserted/text()'`"
+    if [ "$_nprod" -eq "1" ]
+    then
+        info "Product metadata record inserted successfully into the catalogue."
+        exit 0
+    fi
+fi
+
+error "Failed to register the catalogue metadata record of product $IDENTIFIER!"
+exit 1
